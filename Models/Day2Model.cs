b757206978c1ee5f5736b29b5199054466bbf61e
Mike Avgeros <mike.avgeros@sky.uk>
2022-12-06 19:58:40 +0000

    First 4 days

---
+++ Models/Day2Model.cs
@@ -0,0 +1,67 @@
+﻿namespace AoC2022.Models
+{
+    public class Day2Model
+    {
+        public string ColumnCode { get; set; }
+        public string Shape { get; set; }
+        public int ShapePoints { get; set; }
+        public string WinsAgainst { get; set; }
+        public string LosesAgainst { get; set; }
+
+        public Day2Model(string columnCode = "", string shape = "")
+        {
+            if (!string.IsNullOrEmpty(columnCode))
+            {
+                ColumnCode = columnCode;
+                switch (columnCode)
+                {
+                    case "A" or "X":
+                        Shape = "Rock";
+                        ShapePoints = 1;
+                        WinsAgainst = "Scissors";
+                        LosesAgainst = "Paper";
+                        break;
+                    case "B" or "Y":
+                        Shape = "Paper";
+                        ShapePoints = 2;
+                        WinsAgainst = "Rock";
+                        LosesAgainst = "Scissors";
+                        break;
+                    case "C" or "Z":
+                        Shape = "Scissors";
+                        ShapePoints = 3;
+                        WinsAgainst = "Paper";
+                        LosesAgainst = "Rock";
+                        break;
+                }
+            }
+            
+            if (!string.IsNullOrEmpty(shape))
+            {
+                Shape = shape;
+                switch (shape)
+                {
+                    case "Rock":
+                        ColumnCode = "A";
+                        ShapePoints = 1;
+                        WinsAgainst = "Scissors";
+                        LosesAgainst = "Paper";
+                        break;
+                    case "Paper":
+                        ColumnCode = "B";
+                        ShapePoints = 2;
+                        WinsAgainst = "Rock";
+                        LosesAgainst = "Scissors";
+                        break;
+                    case "Scissors":
+                        ColumnCode = "C";
+                        ShapePoints = 3;
+                        WinsAgainst = "Paper";
+                        LosesAgainst = "Rock";
+                        break;
+                }
+            }
+            
+        }
+    }
+}
---
+++ Models/Day2Model2.cs
@@ -0,0 +1,27 @@
+﻿namespace AoC2022.Models
+{
+    public class Day2Model2
+    {
+        public int Points { get; set; }
+
+        public Day2Model2(Day2Model opponent, string strategy)
+        {
+            Day2Model me;
+            switch (strategy)
+            {
+                case "Y":
+                    me = new Day2Model(opponent.ColumnCode);
+                    Points = me.ShapePoints + 3;
+                    break;
+                case "X":
+                    me = new Day2Model(shape: opponent.WinsAgainst);
+                    Points = me.ShapePoints;
+                    break;
+                case "Z":
+                    me = new Day2Model(shape: opponent.LosesAgainst);
+                    Points = me.ShapePoints + 6;
+                    break;
+            }
+        }
+    }
+}
